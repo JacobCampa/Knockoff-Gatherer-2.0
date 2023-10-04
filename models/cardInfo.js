0705d642d6b092dff836ef19d51bace66a90f178
Michael Hannah <Michaelrkhannah@gmail.com>
2023-10-03 20:24:47 -0600

    Added Card Info Model and updated schema to have forign key

---
+++ models/cardInfo.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class CardInfo extends Model {}
+
+// TODO: Add validations to the User model
+
+CardInfo.init(
+  {
+    card_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    card_type: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    rarity: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    edition: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    artist_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'cardinfo',
+  }
+);
+
+module.exports = CardInfo;
