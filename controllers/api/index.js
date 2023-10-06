c2bdc0a41fc6ed30877afad70010c834eba33cb7
Anthony Ciccone <cicconeworldwide@gmail.com>
2023-10-05 19:22:35 -0600

    updated api/index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const loginRoutes = require('./login-routes');
 const logoutRoutes = require('./logout-routes');
+const userRoutes = require('./user-routes')
 
 router.use('/login', loginRoutes);
-route.use('/logout', logoutRoutes);
+router.use('/logout', logoutRoutes);
+router.use('/user', userRoutes);
 
 module.exports = router;
