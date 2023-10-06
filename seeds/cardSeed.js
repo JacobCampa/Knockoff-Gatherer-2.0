9cadd1417f5d01159da10ac69d411c7a54f875fa
Michael Hannah <Michaelrkhannah@gmail.com>
2023-10-05 18:53:39 -0600

    Added seeds and changed password validation in UserInfo.js inside of models

---
+++ seeds/cardSeed.js
@@ -0,0 +1,11 @@
+const ( CardInfo ) = require('../models');
+
+const cardData = [
+    {
+        card_name: 'Earl of Squirrel',
+        card_type: 'Creature',
+        rarity: 'Rare',
+        edition: 'Unstable',
+        artist_name: 'Milivoi Ceran'
+    }
+]
