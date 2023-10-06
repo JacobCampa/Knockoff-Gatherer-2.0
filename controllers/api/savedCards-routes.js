2081df2f4d315ab33ce6f1e0fef765a57f076401
Anthony Ciccone <cicconeworldwide@gmail.com>
2023-10-05 20:07:47 -0600

    edited savedCards route

--- controllers/api/savedCards-routes.js
+++ controllers/api/savedCards-routes.js
@@ -1,4 +1,8 @@
 const router = require("express").Router();
+
+const { cardInfo, userInfo } = require("../../models");
+
+
 // http://localhost:3001/api/deck-routes
 router.get("/api/deck-routes", (req, res) => {
   res.render("login-routes");
@@ -8,47 +12,34 @@ module.exports = router;
 
 
 // GET one card
-router.get('/:id', async (req, res) => {
+router.get('/api/savedCards/:id', async (req, res) => {
     try {
-      const cardData = await User.findByPk(req.params.id);
+      const cardData = await cardInfo.findByPk(req.params.id);
       if (!cardData) {
         res.status(404).json({ message: 'No card with this id!' });
         return;
       }
-      res.status(200).json(userData);
+      res.status(200).json(cardData);
     } catch (err) {
       res.status(500).json(err);
     }
 });
 
-// POST create a new user
-router.post('/', async (req, res) => {
-    try {
-      const userData = await User.create(req.body);
-      res.status(200).json(userData);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
   
-  
-// PUT update a user
-router.put('/:id', async (req, res) => {
-    try {
-      const userData = await User.update(req.body, {
-        where: {
-          id: req.params.id,
-        },
-        individualHooks: true
-      });
-      if (!userData[0]) {
-        res.status(404).json({ message: 'No user with this id!' });
-        return;
-      }
-      res.status(200).json(userData);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+//get all cards
+router.get("/", async (req, res) => {
+  try {
+    const cards = await cardInfo.findAll({
+      include: {
+        model: cardInfo,
+        attributes: ["card_name", "card_type", "rarity", "edition", "artist_name"],
+      },
+    });
+    res.json(cards);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+});
   
   module.exports = router;
