482f25ef527f0946e66c73e815b7146d3010769b
Michael Hannah <Michaelrkhannah@gmail.com>
2023-10-05 19:54:24 -0600

    Updated Port

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,6 +4,6 @@ const loginRoutes = require('./login-routes');
 const logoutRoutes = require('./logout-routes');
 
 router.use('/login', loginRoutes);
-route.use('/logout', logoutRoutes);
+router.use('/logout', logoutRoutes);
 
 module.exports = router;
