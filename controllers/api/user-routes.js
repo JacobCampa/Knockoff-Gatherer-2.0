f70e95632275b1db1fb77283ef805249ac7ebec9
Michael Hannah <Michaelrkhannah@gmail.com>
2023-10-10 20:27:09 -0600

    added working seeds

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -9,7 +9,7 @@ router.get('/user/:id', async (req, res) => {
         res.status(404).json({ message: 'No user with this id!' });
         return;
       }
-      res.render('userData', userData);
+      // res.render('userData', userData);
       res.status(200).json(userData);
     } catch (err) {
       res.status(500).json(err);
@@ -20,7 +20,7 @@ router.get('/user/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
-    res.render('userData', userData);
+    // res.render('userData', userData);
     res.status(200).json(userData);
   } catch (err) {
     res.status(400).json(err);
--- server.js
+++ server.js
@@ -43,6 +43,6 @@ const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
