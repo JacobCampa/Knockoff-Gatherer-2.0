00d112aa57ad3b4314c2d33a8b62d673d58aec45
mono <traineralaina@gmail.com>
2023-10-12 18:22:34 -0600

    fixed issues with handlebars and convereted all html to handlebars

--- controllers/api/collection-routes.js
+++ controllers/api/collection-routes.js
@@ -9,6 +9,7 @@ router.get('/user/:id', async (req, res) => {
         res.status(404).json({ message: 'No user with this id!' });
         return;
       }
+      res.render('collection', { Username: userData.Username });
       res.status(200).json(userData);
     } catch (err) {
       res.status(500).json(err);
