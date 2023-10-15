b1454d90d97f8ef48779e3706577b3eb6a6d303e
Michael Hannah <Michaelrkhannah@gmail.com>
2023-10-15 10:14:52 -0600

    Added comments in connection.js and in all api routes besides logout.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,18 +1,19 @@
+//requires express's router docs
 const router = require('express').Router();
 
+//requires all api routes
 const loginRoutes = require('./login-routes');
 const logoutRoutes = require('./logout-routes');
 const userRoutes = require('./user-routes');
 const savedCards = require('./savedCards-routes');
 const signupRoutes = require('./signup-routes');
 
-
+//tells router how to use the above api routes in the path
 router.use('/login', loginRoutes);
 router.use('/logout', logoutRoutes);
 router.use('/user', userRoutes);
 router.use('/cards', savedCards);
 router.use('/signup', signupRoutes);
-
 
 
 module.exports = router;
