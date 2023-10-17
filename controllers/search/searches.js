668d2f3589aa5a364f34d62826cb3ef907f8a189
Jacob Campa <jacob.campa@outlook.com>
2023-10-16 19:52:28 -0500

    finished api routes and added population to search page to veiw results

--- controllers/search/searches.js
+++ controllers/search/searches.js
@@ -3,21 +3,25 @@ const router = require('express').Router()
 // localhost:33001/searchengine
 router.post('/', async (req, res) => {
     //gets user Input and Option picks ad plugs them into search API
-    const input = req.body.userInput
-    const userOption = req.body.userOption
-    console.log(input)
-    console.log(req.body)
-    const api =  `https://api.scryfall.com/cards/search?q=${input}`
-    console.log(api)
+    const cardName = req.body.inputName
+    const cmc = req.body.inputCmc
+    const color = req.body.inputColor
+    const type = req.body.inputType
+    const subtype = req.body.inputSubtype
+    const rarity = req.body.inputRarity
+    const set = req.body.inputSet
+    const artist = req.body.inputArtist
+    const api =  `https://api.magicthegathering.io/v1/cards?name=${cardName}&cmc=${cmc}&colors=${color}&types=${type}&subtypes=${subtype}&rarity=${rarity}&set=${set}&artist=${artist}`
     fetch(api)
     //Returns JSON from search listed above
     .then((response) => {
+        console.log(api)
         return response.json()
     })
     //grabs JSON and makes Data
     .then((data) => {
-        console.log(data)
         res.json(data)
+        // res.render("searchengine", {data})
     })
 })
 
